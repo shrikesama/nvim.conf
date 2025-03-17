b1667475303d6cadc243473343e2d0861397f6f2
night <night@nightdeMacBook-Pro.local>
2025-03-18 06:04:08 +0800

    feat(markdown): configure render-markdown.nvim code block styling
    - Enable thin borders around code blocks
    - Disable background highlighting for all languages
    - Set horizontal dividers with dash characte

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,6 +1,21 @@
 local renderMD = {
 	"MeanderingProgrammer/render-markdown.nvim",
 	ft = { "markdown", "codecompanion" },
-y}
+	opts = {
+		code = {
+			-- A list of language names for which background highlighting will be disabled.
+			-- Likely because that language has background highlights itself.
+			-- Use a boolean to make behavior apply to all languages.
+			-- Borders above & below blocks will continue to be rendered.
+			disable_background = true,
+
+			border = "thin",
+			-- Used above code blocks for thin border.
+			above = "-",
+			-- Used below code blocks for thin border.
+			below = "-",
+		},
+	},
+}
 
 return { renderMD }
